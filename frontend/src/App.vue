bf8682c875a377948fd705a064cd3f216af49be5
xiaoxu <xufugui88@163.com>
2026-07-28 22:25:05 +0800

    add vue file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,18 @@
   <HeroSection />
   <PainPoints />
   <Comparison />
+  <Competitors />
   <Features />
+  <Architecture />
+  <SqlDemo />
+  <ReportPreview />
+  <Assessment />
+  <Security />
   <Metrics />
   <FlowSteps />
   <Versions />
   <QuickStart />
+  <FAQ />
   <FooterBar />
 </template>
 
@@ -16,11 +23,18 @@ import NavBar from './components/NavBar.vue'
 import HeroSection from './components/HeroSection.vue'
 import PainPoints from './components/PainPoints.vue'
 import Comparison from './components/Comparison.vue'
+import Competitors from './components/Competitors.vue'
 import Features from './components/Features.vue'
+import Architecture from './components/Architecture.vue'
+import SqlDemo from './components/SqlDemo.vue'
+import ReportPreview from './components/ReportPreview.vue'
+import Assessment from './components/Assessment.vue'
+import Security from './components/Security.vue'
 import Metrics from './components/Metrics.vue'
 import FlowSteps from './components/FlowSteps.vue'
 import Versions from './components/Versions.vue'
 import QuickStart from './components/QuickStart.vue'
+import FAQ from './components/FAQ.vue'
 import FooterBar from './components/FooterBar.vue'
 
 import { useTheme } from './composables/useTheme'
