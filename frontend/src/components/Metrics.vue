6403d75775c0a01715b6caafdf6aeb6762e58804
xiaoxu <xufugui88@163.com>
2026-07-29 23:40:53 +0800

    update  Metrics  file

--- frontend/src/components/Metrics.vue
+++ frontend/src/components/Metrics.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section" id="metrics">
     <div class="section-inner">
-      <div class="section-tag">{{ t.metrics.tag }}</div>
+      <div class="section-tag" v-if="t.metrics.tag">{{ t.metrics.tag }}</div>
       <h2 class="section-title">{{ t.metrics.title }}</h2>
       <p class="section-desc"></p>
       <div class="metrics-grid">
