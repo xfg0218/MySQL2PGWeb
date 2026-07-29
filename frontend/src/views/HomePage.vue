d50b7653371aa98b21dc1ccba152c1f96e520af6
xiaoxu <xufugui88@163.com>
2026-07-29 23:45:47 +0800

    add HomePage file

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeroSection />
+  <PainPoints />
+  <Competitors />
+  <Features />
+  <Architecture />
+  <SqlDemo />
+  <ReportPreview />
+  <Assessment />
+  <Security />
+  <Metrics />
+  <FlowSteps />
+  <Versions />
+  <QuickStart />
+</template>
+
+<script setup>
+import HeroSection from '../components/HeroSection.vue'
+import PainPoints from '../components/PainPoints.vue'
+import Competitors from '../components/Competitors.vue'
+import Features from '../components/Features.vue'
+import Architecture from '../components/Architecture.vue'
+import SqlDemo from '../components/SqlDemo.vue'
+import ReportPreview from '../components/ReportPreview.vue'
+import Assessment from '../components/Assessment.vue'
+import Security from '../components/Security.vue'
+import Metrics from '../components/Metrics.vue'
+import FlowSteps from '../components/FlowSteps.vue'
+import Versions from '../components/Versions.vue'
+import QuickStart from '../components/QuickStart.vue'
+</script>
